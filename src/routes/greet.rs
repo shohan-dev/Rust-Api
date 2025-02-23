74f36db3912791913dc828a98634b3c5af1167a4
Md Sabbir Roshid Shohan <59328735+shohan-dev@users.noreply.github.com>
2025-02-23 23:56:27 +0600

    Update

--- src/routes/greet.rs
+++ src/routes/greet.rs
@@ -1,16 +1,6 @@
 use actix_web::{web, HttpResponse, Responder};
-use serde::Serialize;
-
-#[derive(Serialize)]
-struct GreetResponse {
-    message: String,
-}
 
 pub async fn greet(path: web::Path<String>) -> impl Responder {
     let name = path.into_inner();
-    let response = GreetResponse {
-        message: format!("Hello, {}! 👋", name),
-        _Time: format!("Time: {}", chrono::Local::now().naive_local()),
-    };
-    HttpResponse::Ok().json(response)
+    HttpResponse::Ok().body(format!("Hello, {}! 👋", name))
 }
--- src/routes/hello.rs
+++ src/routes/hello.rs
@@ -1,7 +1,5 @@
 use actix_web::{HttpResponse, Responder};
-use serde_json::json;
 
 pub async fn hello() -> impl Responder {
-    let response = json!({ "message": "Hello, World! 🌍","route": "/greet/shohan", "Route is Name" });
-    HttpResponse::Ok().json(response)
+    HttpResponse::Ok().body("Hello, World! 🌍")
 }
